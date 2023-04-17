48fd7f0d19d6cf18f5d2cfb4d16170d12ea68cd0
pgray <p@pgray.org>
2023-04-17 09:11:00 -0400

    top nav tweaks

--- unsigned-fm-client/src/components/shared/top-nav.vue
+++ unsigned-fm-client/src/components/shared/top-nav.vue
@@ -1,13 +1,22 @@
 <template>
-
+  <va-navbar color="primary" class="mb-3">
+    <template #left>
+      <va-navbar-item class="logo"> LOGO </va-navbar-item>
+    </template>
+    <template #right>
+      <va-navbar-item>Dashboard</va-navbar-item>
+      <va-navbar-item>Reports</va-navbar-item>
+      <va-navbar-item>Users</va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-magnify" color="textInverted" />
+      </va-navbar-item>
+      <va-navbar-item>
+        <va-icon name="mdi-account-circle-outline" color="textInverted" />
+      </va-navbar-item>
+    </template>
+  </va-navbar>
 </template>
 
-<script>
-export default {
-  name: "top-nav"
-};
-</script>
+<script setup></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
