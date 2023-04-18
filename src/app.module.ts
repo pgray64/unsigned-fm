8156f4d0522c8c5b7a500dab11b3f36cc00327e3
pgray <p@pgray.org>
2023-04-18 10:08:31 -0400

    fix some stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'unsigned-fm-client/dist'),
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.UFM_PG_USER, // These are only loaded from OS env variables, not env files
+      password: process.env.UFM_PG_PASS,
+      database: process.env.UFM_PG_DB,
+      entities: [],
+      synchronize: false,
+    }),
+    ConfigModule.forRoot({ envFilePath: ['.local.env', '.env'] }),
   ],
   controllers: [AppController],
   providers: [AppService],
