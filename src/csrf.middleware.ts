d94217e192092f2c61c16c3ab369b57fdbeee888
pgray <p@pgray.org>
2023-05-15 16:44:59 -0400

    CSRF protection middleware

---
+++ src/csrf.middleware.ts
@@ -0,0 +1,25 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+const cookieName = 'csrf_token';
+const headerName = 'X-CSRF-TOKEN';
+
+export function CsrfMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (req.method === 'GET') {
+    next();
+    return;
+  }
+  // Check csrf token
+  const cookieValue = req.cookies[cookieName]?.value;
+  const headerValue = req.header(headerName);
+  // careful to not allow when they both match but are unset
+  if (headerValue?.length > 1 && cookieValue === headerValue) {
+    next();
+  } else {
+    throw new UnauthorizedException('Invalid CSRF token');
+  }
+}
